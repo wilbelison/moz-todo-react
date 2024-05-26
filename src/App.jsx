7a7b29ad960d9c30cec21c26adf7cdccbfdfa18c
Wilbelison Junior <meustudio@gmail.com>
2024-05-26 16:58:43 -0300

    filters

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import { nanoid } from 'nanoid'
+
 import Form from './components/Form'
 import FilterButton from './components/FilterButton'
 import Todo from './components/Todo'
 
+const FILTER_MAP = {
+	All: () => true,
+	Active: (task) => !task.completed,
+	Completed: (task) => task.completed,
+}
+
+const FILTER_NAMES = Object.keys(FILTER_MAP)
+
 function App(props) {
 	const [tasks, setTasks] = useState(props.tasks)
+	const [filter, setFilter] = useState('All')
 
 	function addTask(name) {
 		const newTask = { id: `todo-${nanoid()}`, name, completed: false }
@@ -37,17 +47,28 @@ function App(props) {
 		setTasks(editedTasks)
 	}
 
-	const taskList = tasks?.map((task) => (
-		<Todo
-			id={task.id}
-			name={task.name}
-			completed={task.completed}
-			key={task.id}
-			toggleTaskCompleted={toggleTaskCompleted}
-			deleteTask={deleteTask}
-            editTask={editTask}
+	const filterList = FILTER_NAMES.map((name) => (
+		<FilterButton
+			key={name}
+			name={name}
+			isPressed={name === filter}
+			setFilter={setFilter}
 		/>
 	))
+
+	const taskList = tasks
+		.filter(FILTER_MAP[filter])
+		.map((task) => (
+			<Todo
+				id={task.id}
+				name={task.name}
+				completed={task.completed}
+				key={task.id}
+				toggleTaskCompleted={toggleTaskCompleted}
+				deleteTask={deleteTask}
+				editTask={editTask}
+			/>
+		))
 
 	const taskNoun = taskList.length !== 1 ? 'tasks' : 'task'
 	const headingText = `${taskList.length} ${taskNoun} remaing`
@@ -56,11 +77,7 @@ function App(props) {
 		<div className="todoapp stack-large">
 			<h1>TodoMatic</h1>
 			<Form addTask={addTask} />
-			<div className="filters btn-group stack-exception">
-				<FilterButton />
-				<FilterButton />
-				<FilterButton />
-			</div>
+			<div className="filters btn-group stack-exception">{filterList}</div>
 			<h2 id="list-heading">{headingText}</h2>
 			<ul
 				role="list"
