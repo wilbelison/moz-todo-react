c3cb1746fbfccbfa429b96ce7064c2506dc1f06c
Wilbelison Junior <meustudio@gmail.com>
2024-05-26 12:56:36 -0300

    handle events and prettier tabs
    change prettier to use tab indent

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import './index.css'
 
 // eslint-disable-next-line
 const DATA = [
-  { id: 'todo-0', name: 'Eat', completed: true },
-  { id: 'todo-1', name: 'Sleep', completed: false },
-  { id: 'todo-2', name: 'Repeat', completed: false },
+	{ id: 'todo-0', name: 'Eat', completed: true },
+	{ id: 'todo-1', name: 'Sleep', completed: false },
+	{ id: 'todo-2', name: 'Repeat', completed: false },
 ]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App tasks={DATA} />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App tasks={DATA} />
+	</React.StrictMode>
 )
