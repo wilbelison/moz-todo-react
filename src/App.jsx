70c913392cf21c03f51aa541c331dc364d0ec4af
Wilbelison Junior <meustudio@gmail.com>
2024-05-26 13:21:06 -0300

    tasks heading text counter

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,9 @@ function App(props) {
 		/>
 	))
 
+	const taskNoun = taskList.length !== 1 ? 'tasks' : 'task'
+	const headingText = `${taskList.length} ${taskNoun} remaing`
+
 	return (
 		<div className="todoapp stack-large">
 			<h1>TodoMatic</h1>
@@ -30,7 +33,7 @@ function App(props) {
 				<FilterButton />
 				<FilterButton />
 			</div>
-			<h2 id="list-heading">3 tasks remaining</h2>
+			<h2 id="list-heading">{headingText}</h2>
 			<ul
 				role="list"
 				className="todo-list stack-large stack-exception"
