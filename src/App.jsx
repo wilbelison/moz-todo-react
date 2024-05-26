f3467a2c97ab55010254f85e9862fb55022bd7bb
Wilbelison Junior <meustudio@gmail.com>
2024-05-26 15:15:11 -0300

    edit templates

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,16 @@ function App(props) {
 		setTasks(remaingTasks)
 	}
 
+	function editTask(id, newName) {
+		const editedTasks = tasks.map((task) => {
+			if (id === task.id) {
+				return { ...task, name: newName }
+			}
+			return task
+		})
+		setTasks(editedTasks)
+	}
+
 	const taskList = tasks?.map((task) => (
 		<Todo
 			id={task.id}
@@ -35,6 +45,7 @@ function App(props) {
 			key={task.id}
 			toggleTaskCompleted={toggleTaskCompleted}
 			deleteTask={deleteTask}
+            editTask={editTask}
 		/>
 	))
 
