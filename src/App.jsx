966c793fa42ee8c1c074a785412c5a4b81210606
Wilbelison Junior <meustudio@gmail.com>
2024-05-26 13:06:09 -0300

    setTasks useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { useState } from 'react'
 import Form from './components/Form'
 import FilterButton from './components/FilterButton'
 import Todo from './components/Todo'
 
 function App(props) {
+	const [tasks, setTasks] = useState(props.tasks)
+
 	function addTask(name) {
-		alert(name)
+		const newTask = { id: 'id', name, completed: false }
+		setTasks([...tasks, newTask])
 	}
-	const taskList = props.tasks?.map((task) => (
+
+	const taskList = tasks?.map((task) => (
 		<Todo
 			id={task.id}
 			name={task.name}
@@ -14,6 +19,7 @@ function App(props) {
 			key={task.id}
 		/>
 	))
+
 	return (
 		<div className="todoapp stack-large">
 			<h1>TodoMatic</h1>
