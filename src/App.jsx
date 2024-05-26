470e08595492935f3afd1f160e776c31e249f70a
Wilbelison Junior <meustudio@gmail.com>
2024-05-26 17:52:14 -0300

    after delete focus

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { nanoid } from 'nanoid'
 
 import Form from './components/Form'
 import FilterButton from './components/FilterButton'
 import Todo from './components/Todo'
+
+function usePrevious(value) {
+	const ref = useRef()
+	useEffect(() => {
+		ref.current = value
+	})
+	return ref.current
+}
 
 const FILTER_MAP = {
 	All: () => true,
@@ -73,12 +81,23 @@ function App(props) {
 	const taskNoun = taskList.length !== 1 ? 'tasks' : 'task'
 	const headingText = `${taskList.length} ${taskNoun} remaing`
 
+	const listHeadingRef = useRef(null)
+	const prevTaskLength = usePrevious(tasks.length)
+
+	useEffect(() => {
+		if (tasks.length < prevTaskLength) {
+			listHeadingRef.current.focus()
+		}
+	}, [tasks.length, prevTaskLength])
+
 	return (
 		<div className="todoapp stack-large">
 			<h1>TodoMatic</h1>
 			<Form addTask={addTask} />
 			<div className="filters btn-group stack-exception">{filterList}</div>
-			<h2 id="list-heading">{headingText}</h2>
+			<h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
+				{headingText}
+			</h2>
 			<ul
 				role="list"
 				className="todo-list stack-large stack-exception"
